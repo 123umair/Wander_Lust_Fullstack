d6fc3556bc1c5d1590f89f94c93a59d02664dedb
@123umair <khanuumair91@gmail.com>
2026-02-13 00:12:58 +0500

    Fix routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,14 @@
 import './App.css'
 import Display_Listings from './components/Display_Listings/Display_Listings'
 import Display_Content from './components/Display_Content/Display_Content'
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-      <Display_Listings />
       <Routes>
-        <Route path={`/listings/$:id`} element={<Display_Content />} />
+        <Route path="/" element={<Display_Listings />} />
+        <Route path={`/listings/:id`} element={<Display_Content />} />
 
       </Routes>
     </>
--- Frontend/src/components/Display_Content/Display_Content.jsx
+++ Frontend/src/components/Display_Content/Display_Content.jsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import axios from 'axios'
-import {useState,useEffect} from 'react'
-import { useParams } from 'react-router'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 const Display_Content = () => {
-    const { id } = useParams()
-    const [content,setContent] = useState([])
-   
+  const { id } = useParams()
+  const [content, setContent] = useState([])
 
-    useEffect(() => {
-       const fetchContent = async () =>{
+
+  useEffect(() => {
+    const fetchContent = async () => {
       try {
-           const res = await axios.get(`http://localhost:4000/listings/${id}`)
-           setContent(res.data.listings)
+        const res = await axios.get(`http://localhost:4000/listings/${id}`)
+        console.log(res,'res')
+        setContent(res.data.listing)
       } catch (error) {
-        console.log("error",error)
+        console.log("error", error)
       }
     }
-     fetchContent()
-    }, [])
-    
-    return (
+    fetchContent()
+  }, [id])
+
+  return (
     <div>
-     <p>Display_Content</p>
-     {content.map((content)=>
-     { return (
-      <div>
-       <p>{content.title}</p>
-      <p>{content.description}</p>
-      <p>{content.price}</p>
-      <p>{content.location}</p>
-      <p>{content.country}</p>
-      </div>
-     )})} 
+      <p>Display_Content</p>
+      {content && (
+        <div>
+          <p>{content.title}</p>
+          <p>{content.description}</p>
+          <p>{content.price}</p>
+          <p>{content.location}</p>
+          <p>{content.country}</p>
+        </div>
+      )}
+
 
     </div>
   )

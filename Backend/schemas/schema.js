9cf415f67afa1c7578b6ad1c820b5f58ae414688
123Umair <khanuumair91@gmail.com>
2026-03-14 22:53:01 +0500

    Adding Zod schema to the backend schema validation

---
+++ Backend/schemas/schema.js
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+
+export const listingSchema = z.object({
+    listing:z.object({
+        title:z.string(), // required by default
+        description:z.string(),
+       image: z.object({
+            url: z.string().nullable().optional()
+        }).optional(), // means image field  will be empty or will in values or its will not include the values means empty..
+        price: z.number().min(1, "enter the Price."),
+        country:z.string(), 
+        location:z.string(),
+        
+    })
+})

11071f54e81a538bb8c03b988d26a3525151cf5a
123Umair <khanuumair91@gmail.com>
2026-05-15 18:23:19 +0500

    Adding Login with bakcend auth and froontend form also

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { User } from '../Models/user.js'
 import { wrapAsync } from '../../utils/wrapAsync.js'
+import passport from 'passport'
 const router = express.Router()
 
 router.post('/signup',wrapAsync(async(req,res)=>{
@@ -16,4 +17,21 @@ try {
 }))
 
 
+//passport.authenticate() is a middlwear where  he can authenticate the user that i will exist in the database or not therefore for the database work we also use the asyn methods.
+router.post('/login',
+    passport.authenticate('local'),
+    wrapAsync(async(req,res)=>{
+    try {
+        let {username,password} = req.body
+        console.log(req.body)
+
+    } catch (error) {
+        console.log('error',error)
+    }
+}))
 export default router
+
+
+// the middlewear passport.authenticate() will want some perimeters. one is strategy(where we should use the localstrategy,and one is if the user is not authenticated user than it will be redidrect toward login.)
+// local 1st perimeter,
+// {failureRedirect:'/login}, 2nd perimeter

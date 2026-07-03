66ee6d8f60cc3e98c3fa493218173c25216d8333
123Umair <khanuumair91@gmail.com>
2026-07-03 12:25:18 +0500

    Fix the problem of the session

--- Backend/src/routes/reviewsRoutes.js
+++ Backend/src/routes/reviewsRoutes.js
@@ -3,16 +3,16 @@ import { wrapAsync } from '../../utils/wrapAsync.js';
 const router = express.Router({ mergeParams: true });
 import { validateReviews } from '../middlewear/validationreviews.js';
 import { createReview, destoryReviews } from '../Controllers/review.js';
-
+import { LoggedIn } from '../middlewear/LoggedIn.js';
 
 
 
 // show reviews
-router.post('/', validateReviews, wrapAsync(createReview))
+router.post('/', LoggedIn, validateReviews, wrapAsync(createReview))
 
 
 // delete reviews route 
-router.delete('/:reviewId', wrapAsync(destoryReviews))
+router.delete('/:reviewId', LoggedIn, wrapAsync(destoryReviews))
 export default router
 
 
